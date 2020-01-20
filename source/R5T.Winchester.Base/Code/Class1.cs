265d665e8349f1398edd4d83440d04a8aba9c2d4
MinexAutomation <minex.automation@gmail.com>
2020-01-20 00:50:08 -0800

    Added the ITestingDataDirectoryPathProvider service definition interface type.

--- source/R5T.Winchester.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Winchester.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Winchester.Base/Code/Services/Definitions/ITestingtDataDirectoryPathProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Winchester
+{
+    public interface ITestingtDataDirectoryPathProvider
+    {
+        string GetTestingDataDirectoryPath();
+    }
+}
